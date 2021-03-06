1ca86c776555f2f20ae86830834f38ceaed86bf1
gulsah-altiparmak <gulsahaltiparmakk@gmail.com>
2021-03-06 18:35:23 +0300

    Static Tableview Controller

---
+++ README.md
@@ -0,0 +1,2 @@
+# Static-table-view
+ 
---
+++ Turkcell_NavigationView_Odev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Turkcell_NavigationView_Odev
+//
+//  Created by Gulsah Altiparmak on 9.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
